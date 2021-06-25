4b48c84f274d6c35cfc5b66bd50c557b2c652572
Sudheer Singh <sudheersid0710@gmail.com>
2021-06-25 14:35:16 +0530

    files added

---
+++ src/components/Demo2.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Demo2 = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Demo2;
---
+++ src/components/Demo3.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Demo3 = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Demo3;
